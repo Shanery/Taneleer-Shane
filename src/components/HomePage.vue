3aa956ac3db60dd126abda585426ab99904b437c
Shanery <shanery@umich.edu>
2018-04-30 23:37:10 -0400

    Added Filter By Feed
    Added in Filter By Feed
    
    Also Added Vuex State for services.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app" @scroll="bottomVisible">
+    
+    <div id="select-source">
+      <div class="tabs">
+        <button class="tab" v-for="(service, title) in services"
+          :class="{'is-active': title==currentService}"
+          @click="changeService(title)">
+          {{ title }}
+          <font-awesome-icon :icon="service.logo"></font-awesome-icon>
+        </button>
+      </div>
+      
+    </div>
     <!-- Feed -->
     <div class="wall">
       <stack :column-min-width="240" :gutter-width="0" :gutter-height="0" :monitor-images-loaded="true">
-        <stack-item v-for="(post, i) in posts" :key="i">
+        <stack-item v-for="(post, i) in curPosts" :key="i">
           <!-- Post -->
           <thumb-card :post="post" :index="i" 
             @open="expandPost"></thumb-card>
@@ -61,9 +73,9 @@ export default {
   },
   data() {
     return {
-      posts: [],
+      services: this.$store.state.services,
       fetching: false,
-      pages: 0,
+      currentService: "Twitter",
 
       currentPost: null,
       modal: false
@@ -74,23 +86,33 @@ export default {
     window.addEventListener('scroll', () => {
       if (this.bottomVisible() && !this.fetching) {
         this.fetching = true;
-        this.getImages();
+        this.getImages(this.services[this.currentService]);
       }
     })
+
+    for (const service in this.services) {
+      if (this.services.hasOwnProperty(service)) {
+        const element = this.services[service];
+        
+        this.$set(element, 'posts', []);
+        this.$set(element, 'page', 0);
+      }
+    }
+    
   },
   computed: {
     curPost() {
-      var post = this.posts[this.currentPost];
+      const posts = this.services[this.currentService].posts;
+      var post = posts[this.currentPost];
 
       return post;
     },
+    curPosts() {
+      return this.services[this.currentService].posts
+    },
     curServiceLogo() {
       if (!this.curPost) return;
-      let logos = {
-        "Twitter": faTwitter,
-        "Instagram": faInstagram
-      }
-      return logos[this.curPost.service];
+      return this.$store.state.services[this.curPost.service].logo;
     },
     imgUrl() {
       var post = this.curPost;
@@ -108,30 +130,33 @@ export default {
     },
     linkToOriginal() {
       if (this.curPost) return this.curPost.url;
-    }
+    },
   },
 
   methods: {
-    getImages() {
+    getImages(service, serviceName) {
       return new Promise((resolve, reject) => {
         var self = this;
         self.fetching = true;
         
+        console.log(service.name)
+        console.log(service.page)
+        
         axios.get("https://taneleer.composedcreative.com/api/v1/feed/a0329f16-9225-11e6-89bb-296a97b9d609/fabb8b71-496e-11e8-afe9-1253691739d7",
           { 
             params: {
-              "filter[services]": ["Instagram"],
-              "page[number]": self.pages,
+              "filter[services]": [service.name],
+              "page[number]": service.page,
               "page[size]": 30
             }
           })
         .then(function(result) {
           console.log(result);
           let data = result.data.data;
-          self.posts.push(...data);
+          service.posts.push(...data);
 
-          self.pages++;
-          console.log(self.pages);
+          service.page++;
+          console.log(service.page);
           this.populateColumns(data);
         }.bind(this))
         .then(() => new Promise(resolve => {
@@ -165,22 +190,39 @@ export default {
     },
     nextPost() {
       if (this.fetching) return;
-
+      const posts = this.services[this.currentService].posts;
       // Get More Images If End Reached
-      if (this.currentPost >= this.posts.length - 1) {
+      if (this.currentPost >= posts.length - 1) {
         this.fetching = true;
 
-        this.getImages()
+        this.getImages(this.services[this.currentService])
           .then(() => {
             this.currentPost += 1;
           })
       } else {
         this.currentPost += 1;
       }
+    },
+    changeService(service) {
+      this.currentService = service;
     }
   },
   mounted() {
-    this.getImages();
+    this.fetching = true;
+
+    for (const service in this.services) {
+      if (this.services.hasOwnProperty(service)) {
+        const element = this.services[service];
+
+        this.getImages(element);
+      }
+    }
+
+
+
+    
+
+    
   }
   
 }
@@ -190,21 +232,22 @@ export default {
 .modal-card {
   display: flex;
   flex-direction: row;
-  height: 80vh;
-  max-height: 80vh;
   
+  width:860px;
+  max-width: 860px;
+
   background-color: #FFF;
   border-radius: 1em;
-  overflow: hidden;
+  overflow: auto;
 
   .modal-card-image {
     flex-grow: 1;
     width: 50%;
     max-width: 50%;
+    height: 100%;
     img {
+      vertical-align: top;
       width: 100%;
-      height: 100%;
-
       object-fit: cover;
     }
   }
@@ -222,22 +265,43 @@ export default {
   }
 
   .logo-button {
-      background-color: #DDD;
-      color: #555;
-      border-radius: 1em;
-      border: none;
-      padding: 0;
+    background-color: #DDD;
+    color: #555;
+    border-radius: 1em;
+    border: none;
+    padding: 0;
 
-      width: 45px;
-      height: 45px;
+    width: 45px;
+    height: 45px;
 
-      &:hover {
-        cursor: pointer;
-        background-color: #555;;
-        color: #DDD;
-      }
+    &:hover {
+      cursor: pointer;
+      background-color: #555;;
+      color: #DDD;
     }
+  }
   
+}
+
+.tabs {
+  .tab {
+    font-size: 18px;
+
+    padding: 0.5em 1em;
+    border: 2px solid #5070B5;
+    border-radius: 0.5em;
+    margin: 0.5em;
+
+    &.is-active {
+      background-color: #5070B5;
+      border: 0px solid #5070B5;
+    }
+
+    &:hover {
+      cursor: pointer;
+      color: #DDD;
+    }
+  }
 }
 
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 
@@ -9,11 +10,30 @@ Vue.config.productionTip = false
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 
 Vue.component("font-awesome-icon", FontAwesomeIcon)
+Vue.use(Vuex)
+
+import { faTwitter, faInstagram } from '@fortawesome/fontawesome-free-brands'
+
+const store = new Vuex.Store({
+  state: {
+    services: {
+      "Twitter": {
+        name: "Twitter",
+        logo: faTwitter,
+      },
+      "Instagram": {
+        name: "Instagram",
+        logo: faInstagram
+      }
+    }
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
